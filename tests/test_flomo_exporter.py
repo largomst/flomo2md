623c2e586a19ac713b8e3fed19278b3abf71ad0e
largomst <dingrui.zhen@gmail.com>
2024-10-22 17:01:36 +0800

    test: Add test for exporting Flomo with files to JSON

--- tests/test_flomo_exporter.py
+++ tests/test_flomo_exporter.py
@@ -41,3 +41,44 @@ def flome2json(source):
             'files': [],
         }
     ]
+
+
+def test_export_flomo_with_files_to_json():
+    source = """ <div class="memo">
+      <div class="time">2024-10-18 17:55:00</div>
+      <div class="content">
+        <p>#领域/产品/设计</p>
+        <p>
+          形式有时候就是本质。HTML
+          的外观是人产生第一印象的地方。传达出这个网站是否是用心设计过的。
+        </p>
+        <ul>
+          <li>漂亮的设计 https://v.flomoapp.com/mine/?memo_id=MTQwOTY1NTk4</li>
+        </ul>
+      </div>
+      <div class="files">
+        <img
+          src="file/2024-10-18/1081485/1729245370.2828069_6T3Afmea.jpg"
+        /><img
+          src="file/2024-10-18/1081485/1729245370.282824_ccUqi8Dd.jpg"
+        /><img src="file/2024-10-18/1081485/1729245370.282831_RRI2OmuW.jpg" />
+      </div>
+    </div>"""
+    want = [
+        {
+            'time': '2024-10-18 17:55:00',
+            'content': """#领域/产品/设计
+
+形式有时候就是本质。HTML的外观是人产生第一印象的地方。传达出这个网站是否是用心设计过的。
+
+漂亮的设计 https://v.flomoapp.com/mine/?memo_id=MTQwOTY1NTk4
+""",
+            'files': [
+                'file/2024-10-18/1081485/1729245370.2828069_6T3Afmea.jpg',
+                'file/2024-10-18/1081485/1729245370.282824_ccUqi8Dd.jpg',
+                'file/2024-10-18/1081485/1729245370.282831_RRI2OmuW.jpg',
+            ],
+        }
+    ]
+    get = flome2json(source)
+    assert want == get
