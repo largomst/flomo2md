6bff976c6751ea0b85ed7e5746fa1578cb2d5d9f
largomst <dingrui.zhen@gmail.com>
2024-10-23 10:32:10 +0800

    feat: 添加完整的导出函数

--- flomo_exporter/__main__.py
+++ flomo_exporter/__main__.py
@@ -1,3 +1,7 @@
+import argparse
+import os
+import uuid
+
 import markdownify
 from bs4 import BeautifulSoup
 
@@ -35,16 +39,58 @@ def memo2json(memo_div):
 def memo2md(memo_div):
     item = memo2json(memo_div)
     time = item['time']
-    content = item['content']
+    content = item['content'].strip().replace('\\', '')
     files = item['files']
-    images = '\n'.join([f'* ![]({x})' for x in files])
+    if files:
+        images = '\nimages:\n' + ''.join([f'* ![]({x})\n' for x in files])
+    else:
+        images = ''
 
-    return create_md(time, content, images)
+    md = create_md(time, content, images)
+    return md
 
 
 def create_md(time, content, images):
-    return f"""# {time}
+    return (
+        f"""# {time}
+
 {content}
-images:
-{images}
 """
+        + images
+    )
+
+
+def flomo2md(source):
+    bs = BeautifulSoup(source, 'html.parser')
+    result = []
+    for memo in bs.find_all('div', class_='memo'):
+        item = memo2md(memo)
+        result.append(item)
+
+    return result
+
+
+def main():
+    input, output = cli()
+
+    with open(input, encoding='utf-8') as f:
+        source = f.read()
+        mds = flomo2md(source)
+        for md in mds:
+            title = uuid.uuid4().hex + '.md'
+            md_path = os.path.join(output, title)
+            with open(md_path, 'w', encoding='utf-8') as f:
+                f.write(md)
+
+
+def cli():
+    p = argparse.ArgumentParser()
+    p.add_argument('input', type=str)
+    p.add_argument('output', type=str)
+    args = p.parse_args()
+    input = args.input
+    output = args.output
+    return input, output
+
+
+main()
--- tests/test_flomo_exporter.py
+++ tests/test_flomo_exporter.py
@@ -31,7 +31,7 @@ def test_success_export_flomo_html_to_json():
     assert want == get
 
 
-def test_memo_to_md():
+def test_memo_div_has_files_to_md():
     source = """
 <html>
 <body>
@@ -46,12 +46,36 @@ def test_memo_to_md():
 </html>
 """
     want = """# 2024-10-22 13:13:53
+
 A
 
 * B
 
 images:
 * ![](file/a.png)
+"""
+    bs = BeautifulSoup(source, 'html.parser')
+    memo_div = bs.find('div', class_='memo')
+    get = memo2md(memo_div)
+    assert want == get
+
+
+def test_memo_div_has_no_files_to_md():
+    source = """
+<html>
+<body>
+<div class="memo">
+    <div class="time">2024-10-22 13:13:53</div>
+    <div class="content"><p>A</p><ul><li><p>B</p></li></ul></div>
+</div>
+</body>
+</html>
+"""
+    want = """# 2024-10-22 13:13:53
+
+A
+
+* B
 """
     bs = BeautifulSoup(source, 'html.parser')
     memo_div = bs.find('div', class_='memo')
