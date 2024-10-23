ba1962762d5f790b4b334d0f786811364ccdd05f
largomst <dingrui.zhen@gmail.com>
2024-10-23 09:10:53 +0800

    refactor: 提取函数

--- flomo_exporter/__main__.py
+++ flomo_exporter/__main__.py
@@ -6,21 +6,27 @@ def flomo2json(source):
     bs = BeautifulSoup(source, 'html.parser')
     result = []
     for memo in bs.find_all('div', class_='memo'):
-        time = memo.find('div', class_='time').text
-
-        content = memo.find('div', class_='content')
-        markdown = markdownify.markdownify(str(content))
-
-        imgs = memo.find_all('img')
-        links = []
-        for img in imgs:
-            links.append(img['src'])
-
-        item = {
-            'time': time,
-            'content': markdown,
-            'files': links,
-        }
+        item = memo2json(memo)
         result.append(item)
 
     return result
+
+
+def memo2json(memo):
+    time = memo.find('div', class_='time').text
+
+    content = memo.find('div', class_='content')
+    markdown = markdownify.markdownify(str(content))
+
+    imgs = memo.find_all('img')
+    links = []
+    for img in imgs:
+        links.append(img['src'])
+
+    item = {
+        'time': time,
+        'content': markdown,
+        'files': links,
+    }
+
+    return item
